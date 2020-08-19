90590aef899ece5ef02155e7802ff1c9039fcf59
mlsvrts <mlsvrts@protonmail.com>
2020-08-18 23:06:44 -0400

    [UPD] Change colorization of title banner to seperate 'Rust' and 'Ed'

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,14 @@ fn sleepms(ms: u64) {
     std::thread::sleep(std::time::Duration::from_millis(ms));
 }
 
-// Run an alternate screen for printing
-fn alt() {
+// Print the title page an wait for user to type
+fn title() {
     // Get some information about the size of the current terminal
     let size = termion::terminal_size().unwrap();
     let mids = (size.0 / 2, size.1 / 2);
+    let bl = color::Fg(color::Blue);
+    let rd = color::Fg(color::Red);
+    let bk = color::Fg(color::Black);
     
     // Draw on this screen instead of the main terminal
     let mut screen = AlternateScreen::from(stdout());
@@ -32,16 +35,25 @@ fn alt() {
     write!(screen, "{}{}", termion::clear::All, color::Fg(color::Blue)).unwrap();
 
     // Get the start position for line horizontals
-    let xpad = mids.0 - 25;
-    let ypad = mids.1 - 3;
+    let xp = mids.0 - 25;
+    let yp = mids.1 - 3;
 
     // Write the title to the screen 
-    write!(screen, "{}██████╗ ██╗   ██╗███████╗████████╗███████╗██████╗\n", cursor::Goto(xpad, ypad)).unwrap();
-    write!(screen, "{}██╔══██╗██║   ██║██╔════╝╚══██╔══╝██╔════╝██╔══██╗\n", cursor::Goto(xpad, ypad + 1)).unwrap();
-    write!(screen, "{}██████╔╝██║   ██║███████╗   ██║   █████╗  ██║  ██║\n", cursor::Goto(xpad, ypad + 2)).unwrap();
-    write!(screen, "{}██╔══██╗██║   ██║╚════██║   ██║   ██╔══╝  ██║  ██║\n", cursor::Goto(xpad, ypad + 3)).unwrap();
-    write!(screen, "{}██║  ██║╚██████╔╝███████║   ██║██╗███████╗██████╔╝\n", cursor::Goto(xpad, ypad + 4)).unwrap();
-    write!(screen, "{}╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝╚═╝╚══════╝╚═════╝\n", cursor::Goto(xpad, ypad + 5)).unwrap();
+    write!(screen, "{}{}██████╗ ██╗   ██╗███████╗████████╗{}███████╗██████╗\n", 
+           cursor::Goto(xp, yp), rd, bl).unwrap();
+    write!(screen, "{}{}██╔══██╗██║   ██║██╔════╝╚══██╔══╝{}██╔════╝██╔══██╗\n", 
+           cursor::Goto(xp, yp+1), rd, bl).unwrap();
+    write!(screen, "{}{}██████╔╝██║   ██║███████╗   ██║   {}█████╗  ██║  ██║\n", 
+           cursor::Goto(xp, yp+2), rd, bl).unwrap();
+    write!(screen, "{}{}██╔══██╗██║   ██║╚════██║   ██║   {}██╔══╝  ██║  ██║\n", 
+           cursor::Goto(xp, yp+3), rd, bl).unwrap();
+    write!(screen, "{}{}██║  ██║╚██████╔╝███████║   ██║{}██╗{}███████╗██████╔╝\n", 
+           cursor::Goto(xp, yp+4), rd, bk, bl).unwrap();
+    write!(screen, "{}{}╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝{}╚═╝{}╚══════╝╚═════╝\n", 
+           cursor::Goto(xp, yp+5), rd, bk, bl).unwrap();
+
+    // Set the text to default color, and the cursor to the start position
+    write!(screen, "{}{}", cursor::Goto(1, 1), color::Fg(color::Reset));
 
     // Flush the output (display)
     screen.flush().unwrap();
@@ -49,7 +61,7 @@ fn alt() {
 
 fn main() {
     // Print the new screen
-    alt(); 
+    title(); 
     
     // Give it some time to display
     sleepms(6000);
