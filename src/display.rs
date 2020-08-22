727e9285401a15be8ddc218c2cb06717bf842b55
mlsvrts <mlsvrts@protonmail.com>
2020-08-21 20:52:44 -0400

    [UPD] Add 'Display' object 	+ Add new display module to manage screen functions 	+ Implement Display object with basic functionality 		- Generate an alternate screen on creation 		- Clear the screen 		- Get window size information 		- Print the Rust.Ed title

---
+++ src/display.rs
@@ -0,0 +1,81 @@
+// ======================================================================================================================== //
+// ----------------------------------------------------- DISPLAY ---------------------------------------------------------- //
+// ======================================================================================================================== //
+extern crate termion;
+
+use termion::screen::AlternateScreen;
+use termion::{color, cursor};
+
+use std::io::{Write, Stdout, stdout};
+
+pub struct Display {
+    screen: AlternateScreen<Stdout>,
+}
+
+impl Display {
+    pub fn new() -> Display {
+        // Builds a new alternate screen into a Display object
+        return Display { screen: AlternateScreen::from(stdout()) };
+    }
+
+    pub fn clear(&mut self) {
+        // Clear the alternate screen
+        let screen = &mut (*self).screen;
+
+        write!(screen, "{}", termion::clear::All).unwrap();
+        screen.flush().unwrap();
+    }
+
+    pub fn size() -> std::result::Result<(u16, u16), std::io::Error> {
+        // Use termion to return the current terminal window size
+        return termion::terminal_size();
+    }
+
+    fn midpoints() -> (u16, u16) {
+        // Calculate the x and y midpoints based on current window size
+        let window = Display::size().unwrap();
+
+        return (window.0 / 2, window.1 / 2);
+    }
+
+    pub fn title(&mut self) {
+        // Generates the 'Rust.Ed' logo, and prints it in the middle of the window
+
+        // Get information about the current size of the screen
+        let mids = Display::midpoints();
+
+        // Set colors for display
+        let bl = color::Fg(color::Blue);
+        let rd = color::Fg(color::Red);
+        let bk = color::Fg(color::Black);
+
+        // Get the start position for line horizontals
+        let xp = mids.0 - 25;
+        let yp = mids.1 - 3;
+
+        // Get the screen
+        let screen = &mut (*self).screen;
+
+        // Write the title to the screen
+        write!(screen, "{}{}██████╗ ██╗   ██╗███████╗████████╗{}███████╗██████╗\n",
+               cursor::Goto(xp, yp), rd, bl).unwrap();
+        write!(screen, "{}{}██╔══██╗██║   ██║██╔════╝╚══██╔══╝{}██╔════╝██╔══██╗\n",
+               cursor::Goto(xp, yp+1), rd, bl).unwrap();
+        write!(screen, "{}{}██████╔╝██║   ██║███████╗   ██║   {}█████╗  ██║  ██║\n",
+               cursor::Goto(xp, yp+2), rd, bl).unwrap();
+        write!(screen, "{}{}██╔══██╗██║   ██║╚════██║   ██║   {}██╔══╝  ██║  ██║\n",
+               cursor::Goto(xp, yp+3), rd, bl).unwrap();
+        write!(screen, "{}{}██║  ██║╚██████╔╝███████║   ██║{}██╗{}███████╗██████╔╝\n",
+               cursor::Goto(xp, yp+4), rd, bk, bl).unwrap();
+        write!(screen, "{}{}╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝{}╚═╝{}╚══════╝╚═════╝\n",
+               cursor::Goto(xp, yp+5), rd, bk, bl).unwrap();
+
+        // Set the text to default color, and the cursor to the start position
+        write!(screen, "{}{}", cursor::Goto(1, 1), color::Fg(color::Reset)).unwrap();
+
+        // Flush the output (display)
+        screen.flush().unwrap();
+    }
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -5,66 +5,22 @@
 //                               Rust.Ed is a lightweight, portable, rust-based text editor.
 // ________________________________________________________________________________________________________________________ //
 
-// Interface and control terminal
-extern crate termion;
-
-use std::io::{Write, stdout};
-
-use termion::{color, cursor};
-use termion::screen::AlternateScreen;
-
+mod display;
+use display::Display;
 
 // Convient function for sleeping millis
 fn sleepms(ms: u64) {
     std::thread::sleep(std::time::Duration::from_millis(ms));
 }
 
-// Print the title page an wait for user to type
-fn title() {
-    // Get some information about the size of the current terminal
-    let size = termion::terminal_size().unwrap();
-    let mids = (size.0 / 2, size.1 / 2);
-    let bl = color::Fg(color::Blue);
-    let rd = color::Fg(color::Red);
-    let bk = color::Fg(color::Black);
-    
-    // Draw on this screen instead of the main terminal
-    let mut screen = AlternateScreen::from(stdout());
-
-    // Clear screen and set cursor position
-    write!(screen, "{}{}", termion::clear::All, color::Fg(color::Blue)).unwrap();
-
-    // Get the start position for line horizontals
-    let xp = mids.0 - 25;
-    let yp = mids.1 - 3;
-
-    // Write the title to the screen 
-    write!(screen, "{}{}██████╗ ██╗   ██╗███████╗████████╗{}███████╗██████╗\n", 
-           cursor::Goto(xp, yp), rd, bl).unwrap();
-    write!(screen, "{}{}██╔══██╗██║   ██║██╔════╝╚══██╔══╝{}██╔════╝██╔══██╗\n", 
-           cursor::Goto(xp, yp+1), rd, bl).unwrap();
-    write!(screen, "{}{}██████╔╝██║   ██║███████╗   ██║   {}█████╗  ██║  ██║\n", 
-           cursor::Goto(xp, yp+2), rd, bl).unwrap();
-    write!(screen, "{}{}██╔══██╗██║   ██║╚════██║   ██║   {}██╔══╝  ██║  ██║\n", 
-           cursor::Goto(xp, yp+3), rd, bl).unwrap();
-    write!(screen, "{}{}██║  ██║╚██████╔╝███████║   ██║{}██╗{}███████╗██████╔╝\n", 
-           cursor::Goto(xp, yp+4), rd, bk, bl).unwrap();
-    write!(screen, "{}{}╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝{}╚═╝{}╚══════╝╚═════╝\n", 
-           cursor::Goto(xp, yp+5), rd, bk, bl).unwrap();
-
-    // Set the text to default color, and the cursor to the start position
-    write!(screen, "{}{}", cursor::Goto(1, 1), color::Fg(color::Reset));
-
-    // Flush the output (display)
-    screen.flush().unwrap();
-}
 
 fn main() {
-    // Print the new screen
-    title(); 
-    
+
+    let mut main = Display::new();
+
+    main.clear();
+    main.title();
+
     // Give it some time to display
     sleepms(6000);
 }
-
-
